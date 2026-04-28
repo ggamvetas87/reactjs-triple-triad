499f98b3fea0379e24d11e6909232c4f98db4199
Yiannis Gamvetas <yiannis.gamvetas@vodafone.com>
2026-04-28 12:44:56 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,7 @@ export default function App() {
         <a href="https://finalfantasy.fandom.com/wiki/Triple_Triad" 
         target="_blank">ℹ️ instructions read here.</a></p>
       <p>Check project on <a href="https://github.com/ggamvetas87/reactjs-triple-triad" target="_blank">Github</a></p>
-      <p>Click the button below to start the game.</p>
-      <p><button onClick={!hasStarted ? () => startGame("single") : () => setInfoModalVisible(false)}>{!hasStarted ? "Start Game (1vCPU)" : "Close"}</button></p>
+      {!hasStarted && <p>Click the button below to start the game.</p>}
       {!hasStarted && <p><button onClick={() => startGame("multiplayer")}>Start Game (1v1)</button></p>}
     </>
   );
